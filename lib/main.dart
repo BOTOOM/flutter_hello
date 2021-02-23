fc973e14e44ba8fff05ec7565796545781c67712
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-02-22 22:16:38 -0500

    primera paantalla

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Hello Fluter App',
-    home: new Material(
-      color: Colors.deepPurple,
-      child: Center(
-        child: new Text(
-          'Hello Flutter!',
-          textDirection: TextDirection.ltr,
+      title: 'Hello Fluter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title in app bar  '),
         ),
-      ),
-    ),
-  ));
+        body: Material(
+          color: Colors.deepPurple,
+          child: Center(
+            child: new Text(
+              'Hello Flutter!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 36.0),
+            ),
+          ),
+        ),
+      )));
 }
