9627d5f217b19a81cdc168c72648d01442381fb8
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-02-24 23:59:46 -0500

    columnas y filas

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -4,17 +4,64 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      child: Text(
-        'Pizza',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 50.0,
-            decoration: TextDecoration.none,
-            fontFamily: 'Oxygen',
-            fontWeight: FontWeight.normal),
-      ),
-    );
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 30.0, left: 10.0),
+        color: Colors.deepOrangeAccent,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Margarita',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Tomate, Mozarela, Masa',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    'Marinara',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'Tomate, Ajo',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Oxygen',
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
